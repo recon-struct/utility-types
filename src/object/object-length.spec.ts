6800a111fc814832002079675c99c8cc2ce8005e
Hans Oksendahl <hansoksendahl@gmail.com>
2024-05-23 00:06:37 -0700

    added a unit test

--- src/object/object-length.spec.ts
+++ src/object/object-length.spec.ts
@@ -6,11 +6,13 @@ import type { ObjectLength } from '~/object/object-length'
 describe('ObjectLength', () => {
   it('returns number of keys in an object', () => {
     type Ex1 = ObjectLength<{ a: 1; b: 2 }>
-    type Ex2 = ObjectLength<{ [k: string]: number }>
-    type Ex3 = ObjectLength<{ a: 1; b: 2; [k: string]: number }>
+    type Ex2 = ObjectLength<{ a: 1; b: 2; c: { d: 3 } }>
+    type Ex3 = ObjectLength<{ [k: string]: number }>
+    type Ex4 = ObjectLength<{ a: 1; b: 2; [k: string]: number }>
 
     type TestEx1 = Expect<IsEqual<Ex1, 2>>
-    type TestEx2 = Expect<IsEqual<Ex2, number>>
+    type TestEx2 = Expect<IsEqual<Ex2, 3>>
     type TestEx3 = Expect<IsEqual<Ex3, number>>
+    type TestEx4 = Expect<IsEqual<Ex4, number>>
   })
 })
