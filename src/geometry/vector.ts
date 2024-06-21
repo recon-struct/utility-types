33e231ec06e952ac44c546e42fcbb9c7365d2553
Hans Oksendahl <hansoksendahl@gmail.com>
2024-06-21 09:07:26 -0700

    added vector

--- src/geometry/vector.ts
+++ src/geometry/vector.ts
@@ -3,6 +3,7 @@
  *
  * @typeParam X - The type of the x-coordinate.
  * @typeParam Y - The type of the y-coordinate.
+ * @group Geometry
  */
 export type Vector<X extends number = number, Y extends number = number> = [
   X,
